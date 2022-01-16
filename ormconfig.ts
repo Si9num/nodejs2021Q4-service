167fd5b968ce79e72fb66f43f4c5ba29c4f4c254
Si9num <78493168+Si9num@users.noreply.github.com>
2022-01-16 19:49:57 +0300

    fix: fix docker

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,7 +4,7 @@ import { User } from './src/resources/users/user.memory.repository';
 
 const config = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
